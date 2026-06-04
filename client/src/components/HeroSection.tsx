1b838eb67ba812ccbaab8540d4148fa71e13a070
DaVitBack154 <68261155+DaVitBack154@users.noreply.github.com>
2026-06-04 14:25:42 +0700

    fdfdf

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -79,13 +79,12 @@ export default function HeroSection() {
         </Flex>
       </Box>
 
-      <Box position="absolute" bottom={0} left={0} right={0}>
+      <Box className="hero-wave" aria-hidden="true">
         <svg
           viewBox="0 0 1440 80"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           preserveAspectRatio="none"
-          style={{ display: 'block' }}
         >
           <path
             d="M0,80 L0,40 Q360,0 720,40 Q1080,80 1440,40 L1440,80 Z"
